c8649307ac67117bb04e50cd6cacfcc8fcc59e65
Brian Otieno <jumaxbrian@gmail.com>
2018-08-02 22:10:46 +0300

    add getters, setters and their tests

--- loan.py
+++ loan.py
@@ -1,27 +1,50 @@
 import datetime
 import logging
 
-#Setup for logging
-logging.basicConfig(filename='app.log',
+# Setup for logging
+logging.basicConfig(
+    filename='app.log',
     format='%(asctime)s:%(levelname)s:%(message)s'
 )
+
 
 class Loan:
     '''
     Loan represents a single loan transaction i.e. line in Loans.csv
     '''
+
     def __init__(self, msisdn, network, loan_date, product, amount):
         self.msisdn = msisdn
-        self.network = network 
+        self.network = network
         self.loan_date = Loan.convertStringToDate(loan_date)
         self.product = product
         self.amount = amount
 
-    def displayIn(self):
-        print('{},{},{},{}'.format(self.network, self.product, self.getMonth(), self.amount))
+    def display(self):
+        return(
+            '{},{},{},{}'
+            .format(
+                self.network,
+                self.product,
+                self.getMonth(),
+                self.amount
+            )
+        )
 
     def getMonth(self):
         return self.loan_date.strftime('%b')
+
+    def getNetwork(self):
+        return self.network
+
+    def getProduct(self):
+        return self.product
+
+    def getAmount(self):
+        return self.amount
+
+    def updateAmount(self, amount):
+        self.amount += amount
 
     @staticmethod
     def isDateValid(date_string):
@@ -47,7 +70,3 @@ class Loan:
     @staticmethod
     def convertStringToDate(date_string):
         return datetime.datetime.strptime(date_string, '%d-%b-%Y')
-
-    
-
-
--- test_loan.py
+++ test_loan.py
@@ -3,17 +3,89 @@ import datetime
 
 from loan import Loan
 
+
 class TestLoan(unittest.TestCase):
+    def test_get_amount(self):
+        tempLoan = Loan(
+            1000000,
+            'Network 1',
+            '12-Mar-2016',
+            'Loan Product 1',
+            1000.50
+        )
+        self.assertEqual(tempLoan.getAmount(), 1000.50)
+
+    def test_get_network(self):
+        tempLoan = Loan(
+            1000000,
+            'Network 1',
+            '12-Mar-2016',
+            'Loan Product 1',
+            1000.50
+        )
+        self.assertEqual(tempLoan.getNetwork(), 'Network 1')
+
+    def test_get_product(self):
+        tempLoan = Loan(
+            1000000,
+            'Network 1',
+            '12-Mar-2016',
+            'Loan Product 1',
+            1000.50
+        )
+        self.assertEqual(tempLoan.getProduct(), 'Loan Product 1')
+
     def test_get_month(self):
-        tempLoan = Loan(1000000, 'Network 1', '12-Mar-2016', 'Loan Product 1', 1000.50)
+        tempLoan = Loan(
+            1000000,
+            'Network 1',
+            '12-Mar-2016',
+            'Loan Product 1',
+            1000.50
+        )
         self.assertEqual(tempLoan.getMonth(), 'Mar')
 
+    def test_update_amount(self):
+        tempLoan = Loan(
+            1000000,
+            'Network 1',
+            '12-Mar-2016',
+            'Loan Product 1',
+            1000.50
+        )
+        tempLoan.updateAmount(2000.50)
+        self.assertEqual(tempLoan.getAmount(), 3001)
+
+    def test_display(self):
+        tempLoan = Loan(
+            1000000,
+            'Network 1',
+            '12-Mar-2016',
+            'Loan Product 1',
+            1000.50
+        )
+        self.assertEqual(
+            tempLoan.display(),
+            'Network 1,Loan Product 1,Mar,1000.5'
+        )
+
     def test_convert_wrong_string_to_date(self):
-        self.assertRaises(ValueError, Loan, 1000000, 'Network 1', '12-Mak-2016', 'Loan Product 1', 1000.50)
-    
+        self.assertRaises(
+            ValueError,
+            Loan,
+            1000000,
+            'Network 1',
+            '12-Mak-2016',
+            'Loan Product 1',
+            1000.50
+        )
+
     def tests_convert_right_string_to_date(self):
         date_string = '12-Mar-2016'
-        self.assertEqual(Loan.convertStringToDate(date_string), datetime.datetime.strptime(date_string, '%d-%b-%Y'))
+        self.assertEqual(
+            Loan.convertStringToDate(date_string),
+            datetime.datetime.strptime(date_string, '%d-%b-%Y')
+        )
 
     def test_if_date_is_valid(self):
         self.assertTrue(Loan.isDateValid('12-Mar-2016'))
@@ -22,6 +94,7 @@ class TestLoan(unittest.TestCase):
     def test_if_amount_is_valid(self):
         self.assertTrue(Loan.isAmountValid(4000))
         self.assertFalse(Loan.isAmountValid('45i'))
+
 
 if __name__ == "__main__":
     unittest.main()
